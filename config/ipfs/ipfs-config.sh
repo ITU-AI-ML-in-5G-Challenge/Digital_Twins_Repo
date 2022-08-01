dfb01c9758eae58b0765e6963033fb1087dcffcf
ar-jaime <ar-j.fusterdelafuent@rakuten.com>
2022-08-01 18:17:42 +0900

    initial commit

---
+++ config/ipfs/ipfs-config.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -ex
+
+ipfs bootstrap rm all
+#ipfs bootstrap add /ip4/172.16.239.11/tcp/4001/p2p/12D3KooWLDTzM1w1b33hGukQTLjZ8PfRHkQaTuxUm29m1Ht91emg
+#ipfs bootstrap add /ip4/172.16.239.12/tcp/4001/p2p/12D3KooWJK5ex3kHibTBSLKAKR61bnF7MxzGtMumFyyV7YddJqUQ
+
