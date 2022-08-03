0956e394ecdcd13068f2e6809d6c514a28542aa3
ar-jaime <ar-j.fusterdelafuent@rakuten.com>
2022-08-03 15:38:05 +0900

    refactor: copy swarm.key to /data/ipfs/folder

--- config/ipfs/ipfs-config.sh
+++ config/ipfs/ipfs-config.sh
@@ -5,3 +5,4 @@ ipfs bootstrap rm all
 #ipfs bootstrap add /ip4/172.16.239.11/tcp/4001/p2p/12D3KooWLDTzM1w1b33hGukQTLjZ8PfRHkQaTuxUm29m1Ht91emg
 #ipfs bootstrap add /ip4/172.16.239.12/tcp/4001/p2p/12D3KooWJK5ex3kHibTBSLKAKR61bnF7MxzGtMumFyyV7YddJqUQ
 
+cp swarm.key /data/ipfs/
