d30c60724acd94bf5f5246ef312d6da7b9b8335c
ar-alv <ar-alv.pendasrecondo@rakuten.com>
2022-08-03 19:12:33 +0900

    Refactor: change clean.sh names

--- clean.sh
+++ clean.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-docker rmi besu-qbft_cur-ctr-mkp
-docker rmi besu-qbft_evol-ctr-mkp
-docker rmi besu-qbft_exp-mg-mkp
-docker rmi besu-qbft_evol-ctr-mkp
-docker rmi besu-qbft_evol-ctr-py
-docker rmi besu-qbft_dt-py
-docker rmi besu-qbft_exp-mg-py
-docker rmi besu-qbft_cur-ctr-py
-docker rmi besu-qbft_cur-ctr-ipfs
-docker rmi besu-qbft_dt-ipfs
-docker rmi besu-qbft_evol-ctr-ipfs
-docker rmi besu-qbft_exp-mg-ipfs
+docker rmi docker-network_cur-ctr-mkp
+docker rmi docker-network_evol-ctr-mkp
+docker rmi docker-network_exp-mg-mkp
+docker rmi docker-network_evol-ctr-mkp
+docker rmi docker-network_evol-ctr-py
+docker rmi docker-network_dt-py
+docker rmi docker-network_exp-mg-py
+docker rmi docker-network_cur-ctr-py
+docker rmi docker-network_cur-ctr-ipfs
+docker rmi docker-network_dt-ipfs
+docker rmi docker-network_evol-ctr-ipfs
+docker rmi docker-network_exp-mg-ipfs
