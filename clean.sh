6ccd9c14605bd2ec71e9df1bb2b52feb678cbae4
ar-jaime <ar-j.fusterdelafuent@rakuten.com>
2022-08-03 17:58:49 +0900

    test: add image clean shell

---
+++ clean.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+docker rmi besu-qbft_cur-ctr-mkp
+docker rmi besu-qbft_evol-ctr-mkp
+docker rmi besu-qbft_exp-mg-mkp
+docker rmi besu-qbft_evol-ctr-mkp
+docker rmi besu-qbft_evol-ctr-py
+docker rmi besu-qbft_dt-py
+docker rmi besu-qbft_exp-mg-py
+docker rmi besu-qbft_cur-ctr-py
+docker rmi besu-qbft_cur-ctr-ipfs
+docker rmi besu-qbft_dt-ipfs
+docker rmi besu-qbft_evol-ctr-ipfs
+docker rmi besu-qbft_exp-mg-ipfs
